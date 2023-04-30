6a8d8c676f0e1370a5022f3f56c05086228609b5
Konsttantin <Ryabovkostya91@icloud.com>
2023-04-30 23:41:42 +0300

    added homework tasks

---
+++ task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите положительное минимальное число: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите положительное максимальное число: ");
+int n = int.Parse(Console.ReadLine());
+
+void Recursion(int m, int n, int summ)
+{
+    if (m > n)
+    {
+        Console.WriteLine(summ);
+        return;
+    }
+    summ += m++;
+    Recursion(m, n, summ);
+}
+
+Recursion(m, n, 0);
---
+++ task3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью
+// рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 29
+
+int m = 3;
+int n = 6;
+Ack(m,n);
+
+int Ack(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m > 0 && n == 0) return Ack(m - 1, 1);
+    if (m > 0 && n > 0) return Ack(m - 1, Ack(m, n - 1));
+    return Ack(m, n);
+}
+
+System.Console.WriteLine(Ack(m, n));
+
